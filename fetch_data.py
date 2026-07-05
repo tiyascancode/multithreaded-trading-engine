0ee0a2d65c37cc60bd9d220d871d861f26e4ac18
Tiyas Ghosh <3179859g@student.gla.ac.uk>
2026-07-05 22:57:02 +0100

    Feature: Add fixed data ingestion script and configure gitignore for data files

--- fetch_data.py
+++ fetch_data.py
@@ -1,9 +1,10 @@
 import yfinance as yf
 import pandas as pd
+import os
 
 def download_data(ticker, start_date, end_date):
     print(f"Downloading data for {ticker} from {start_date} to {end_date}")
-    data = yf.download(ticker, start=start_date, end=end_date) # Fetch historical data from Yahoo Finance
+    data = yf.download(ticker, start=start_date, end=end_date, multi_level_index=False) # Fetch historical data from Yahoo Finance
 
     if data.empty:
         print(f"No data found for {ticker} between {start_date} and {end_date}.")
@@ -17,7 +18,8 @@ def download_data(ticker, start_date, end_date):
     cleaned_data.to_csv(filename)
     print(f"Successfully saved to {filename}\n")
 
-    if __name__ == "__main__":
-        # Get last three years of data
-        download_data(ticker="AAPL", start_date="2023-01-01", end_date="2026-06-01")
-        download_data(ticker="BTC-USD", start_date="2023-01-01", end_date="2026-06-01")
+if __name__ == "__main__":
+    # Get last three years of data
+    download_data(ticker="AAPL", start_date="2023-01-01", end_date="2026-06-01")
+    download_data(ticker="BTC-USD", start_date="2023-01-01", end_date="2026-06-01")
+    print(os.getcwd())
